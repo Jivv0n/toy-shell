14099883ea34c8d4f15690d16951e6a655cb7b9e
Jivv0n <84502236+Jivv0n@users.noreply.github.com>
2021-05-23 01:08:05 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # toy-shell
+
+OS Environment : Linux
+
+So that my shell looks like real shell, I included username, hostname and print working directory and modified each color.
+
+<Built-in command>
+
+1) cd
+ If input is "cd [directory path]", you can change directory. 
+
+2) exit
+ If input is "exit", shell is terminated.
+
+<External command>
+
+1) ls
+ print list
+2) pwd
+ print working directory
+3) date
+ show urrent time
+4) echo
+ echo [...] : print ...
+5) vi
+ vi [file name] : open file
+6) cat
+ cat [file name] : print file on shell
+7) clear
+ clear shell
+
+<Limitations & Problems>
+
+1) Home directory of real shell shows "~", but can't hide home directory. (for me, /home/jiwon)
+
+2) In command of "cd", just typing "cd" can't go home directory.
+
+3) echo prints just one word without word spacing.
+
+4) "clear" operates normally, but it can work through not exec system call but system function. If I use exec system, there is a error like "TERM environment variable not set." 
+
+
--- shell.c
+++ shell.c
@@ -5,36 +5,63 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <pwd.h>
 
 #define MAX_LEN_LINE    100
+#define LEN_HOSTNAME	30
 
 int main(void)
 {
     char command[MAX_LEN_LINE];
-    char *args[] = {command, NULL};
+    char *arg;
+    char *args[] = {command, arg, NULL};
     int ret, status;
     pid_t pid, cpid;
+  
+    char hostname[LEN_HOSTNAME + 1];
+    memset(hostname, 0x00, sizeof(hostname));
+    gethostname(hostname, LEN_HOSTNAME);
     
     while (true) {
         char *s;
         int len;
         
-        printf("MyShell $ ");
-        s = fgets(command, MAX_LEN_LINE, stdin);
+        char pwd[100];
+        getcwd(pwd, 100);
+
+        printf("\033[92;1m%s@%s\033[0m:\033[94;1m%s\033[0m$ ",getpwuid(getuid())->pw_name,hostname,pwd);
+        
+        s = fgets(command, MAX_LEN_LINE, stdin);            
+        
         if (s == NULL) {
             fprintf(stderr, "fgets failed\n");
             exit(1);
         }
         
+        
         len = strlen(command);
-        printf("%d\n", len);
         if (command[len - 1] == '\n') {
             command[len - 1] = '\0'; 
         }
         
-        printf("[%s]\n", command);
-      
+        arg = strtok(command, " ");
+        
+        if (strcmp(arg, "cd") == 0){ // cd
+            arg = strtok(NULL, " ");
+            if (chdir(arg) != 0){
+                printf("Can't Change Directory\n");
+                continue;
+            }
+            else continue;
+        }
+                        
+        else if (strcmp(arg, "exit") == 0){ // exit
+            printf("Exit Shell\n");
+            exit(0);
+        }
+
         pid = fork();
+        
         if (pid < 0) {
             fprintf(stderr, "fork failed\n");
             exit(1);
@@ -49,13 +76,47 @@ int main(void)
                 printf("Exit status is %d\n", WEXITSTATUS(status)); 
             }
         }
-        else {  /* child */
-            ret = execve(args[0], args, NULL);
-            if (ret < 0) {
-                fprintf(stderr, "execve failed\n");   
-                return 1;
-            }
-        } 
+        else {  /* child */	    
+            if (strcmp(arg, "ls") == 0){  // ls
+                arg = strtok(NULL, " ");
+                args[0] = "/bin/ls";
+                args[1] = arg;
+            }	    
+	    	else if (strcmp(arg, "pwd") == 0){ // pwd
+	    		args[0] = "/bin/pwd";
+	    		args[1] = NULL;
+	    	}
+	    	else if (strcmp(arg, "date") == 0){ // date
+	    		args[0] = "/bin/date";
+	    		args[1] = NULL;
+	    	}
+	    	else if (strcmp(arg, "echo") == 0){ // echo
+	    	    arg = strtok(NULL, " ");
+	    	    args[0] = "/bin/echo";
+	    	    args[1] = arg;
+	    	}
+	    	else if (strcmp(arg, "vi") == 0){ // vi
+	    	    arg = strtok(NULL, " ");
+	    	    args[0] = "/usr/bin/vi";
+	    	    args[1] = arg;
+	    	}    	
+	    	else if (strcmp(arg, "cat") == 0){ // cat
+	    	    arg = strtok(NULL, " ");
+	    	    args[0] = "/bin/cat";
+	    	    args[1] = arg;
+	    	} 
+	    	
+	    	ret = execve(args[0], args, NULL);
+	    	if (strcmp(arg, "clear") == 0){ // clear
+	    	    system("clear");
+	    	    ret = 0;
+	    	}
+	    	
+	    	if (ret < 0) {
+	    	    fprintf(stderr, "execve failed\n");   
+	            return 1;
+            	}
+        }
     }
     return 0;
 }
